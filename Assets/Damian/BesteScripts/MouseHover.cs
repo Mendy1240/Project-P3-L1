025d9224b690a948c5aced6bbb9023902828e3fc
dimma-elg <damdam1703@outlook.com>
2024-03-07 13:53:27 +0100

    UIButtons

---
+++ Assets/Damian/BesteScripts/MouseHover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Vector3 startTextSize;
+    public void Start()
+    {
+        startTextSize = transform.localScale;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale *= 1.1f;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = startTextSize;
+    }
+}
+

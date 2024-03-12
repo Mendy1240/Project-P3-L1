83184fc3fb3e07378dcb9d62288304d044f787d5
dimma-elg <damdam1703@outlook.com>
2024-03-12 16:44:01 +0100

    GUN

--- Assets/Damian/BesteScripts/NewBehaviourScript.cs
+++ Assets/Damian/BesteScripts/NewBehaviourScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
-    // Update is called once per frame
+    
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("WeaponInspect");
+            animator.ResetTrigger("idle");
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Shooting");
+            animator.ResetTrigger("idle");
+        }
     }
 }

f26f726c806e34c39dc2cb7def7b865141717f55
dimma-elg <damdam1703@outlook.com>
2024-03-19 14:00:13 +0100

    pausemenu

---
+++ Assets/Damian/BesteScripts/OpenPauseMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPauseMenu : MonoBehaviour
+{
+    
+    public GameObject menu;
+    public bool isMenuOpen;
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isMenuOpen = !isMenuOpen;
+            menu.SetActive(isMenuOpen);
+
+
+            if (isMenuOpen)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Time.timeScale = 0;
+            }
+            if (!isMenuOpen)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+}

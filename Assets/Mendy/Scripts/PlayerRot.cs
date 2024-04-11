fa3e9b7b42f81218c89da67f7a17bab323c7b673
Mendy1240 <mendy.pasman@gmail.com>
2024-04-11 15:19:20 +0200

    rotation

--- Assets/Mendy/Scripts/PlayerRot.cs
+++ Assets/Mendy/Scripts/PlayerRot.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerRot : MonoBehaviour
 {
-    public float mouseX;
+        public Transform head;
     public float mouseY;
-    public float mouseSen;
+    public Vector3 headRot;
+    public float rotSpeed;
+    public float maxRotationX = 60f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,23 @@ public class PlayerRot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       mouseX = Input.GetAxis("Mouse X") * mouseSen;
-       mouseY = Input.GetAxis("Mouse Y") * mouseSen;
+         mouseY = Input.GetAxis("Mouse Y");
+        headRot.x = -mouseY * rotSpeed * Time.deltaTime;
+
+        Vector3 currentRotation = head.localRotation.eulerAngles;
+
+        head.Rotate(headRot);
+
+        float newRotationX = ClampRotation(head.localRotation.eulerAngles.x);
+
+        head.localRotation = Quaternion.Euler(newRotationX, currentRotation.y, currentRotation.z);
+    }
+
+    // Methode om de rotatie te beperken tot het bereik van -maxRotationX tot maxRotationX
+    float ClampRotation(float rotation)
+    {
+        if (rotation > 180)
+            rotation -= 360;
+        return Mathf.Clamp(rotation, -maxRotationX, maxRotationX);
     }
 }
